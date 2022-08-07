a8773a63c8ea457b73fad26dd5f213e4c50475bc
Luis Chilo <gfxargentina@gmail.com>
2022-08-07 17:58:15 -0300

    Add: basic server setup

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 //servidor
+const express = require('express');
+const router = require('./router');
+
+const app = express();
+
+const port = 3005;
+
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo el puerto ${port}`);
+});
--- router.js
+++ router.js
@@ -1,1 +1,9 @@
 //router
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('1era ruta del servidor');
+});
+
+module.exports = router;
